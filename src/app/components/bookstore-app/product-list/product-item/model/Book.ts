73ff465a2f4ab042dd5991821090c85aeaa8091f
Silvio Pochini <101467315+n1ghtk1ll3r@users.noreply.github.com>
2022-08-04 11:30:59 -0300

    Add files via upload

---
+++ src/app/components/bookstore-app/product-list/product-item/model/Book.ts
@@ -0,0 +1,8 @@
+export class Book{
+    id!:string;
+    name!:string;
+    price!:string;
+    quantity!:number;
+    category!:string;
+    img!:string;
+}
---
+++ src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Book } from "./product-item/model/Book";
+
+@Injectable()
+
+export class BookService
+{
+    private url = 'https://localhost:44382/api/bookstore';
+
+    httpOptions= {
+        Headers: new HttpHeaders({'content-type': 'application/json'})
+    }
+
+    constructor(private http:HttpClient){}
+
+    getBook(){
+        return this.http.get(this.url)
+    }
+}
